5a7d650c223080cb5f83b8e5cac3c987bbf87249
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-03-05 18:38:36 +0530

    user cration done at CreateAccountActivity

--- app/src/main/java/com/example/twitter/CreateAccountActivity.java
+++ app/src/main/java/com/example/twitter/CreateAccountActivity.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,8 +28,27 @@ public class CreateAccountActivity extends AppCompatActivity {
     EditText dob;
     EditText phone;
 
-    public void createUspa(View view)
+    public void createUser(View view)
     {
+        ParseUser user = new ParseUser();
+        user.put("name",fullname.getText());
+        user.put("phone",phone.getText());
+        user.put("dob",dob.getText());
+
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e== null) {
+                    Log.i("User creation", "Successful!");
+                    Toast.makeText(CreateAccountActivity.this,"create username and password",Toast.LENGTH_LONG).show();
+                } else {
+                    Log.i("Parse Result", "Failed" + e.toString());
+                    Toast.makeText(CreateAccountActivity.this,"Error",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
         Intent intent= new Intent(this,UsernamePasswordActivity.class);
         startActivity(intent);
 
